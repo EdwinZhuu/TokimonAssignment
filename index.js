3656e2632eaefb0270c2fcde675d8da9432a1b47
Edwin Zhu <Edwinhzhu@Hotmail.com>
2019-10-14 00:53:34 -0700

    Back end done

--- index.js
+++ index.js
@@ -14,29 +14,38 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-	.get('/test', async (req, res) => {
+	.get('/mainpage', async (req, res) => {
 		const client = await pool.connect()
-		const tokiresult = await client.query('SELECT * FROM Tokimon');
+		const tokiresult = await client.query('SELECT name FROM Tokimon');
 		const tokiresults = { 'tokiresults': (tokiresult) ? tokiresult.rows : null};
-		res.render('pages/test', tokiresults)
+		res.render('pages/mainpage', tokiresults)
 		client.release();
 	})
-  .post('/test', async (req, res) => {
+  .post('/mainpage', async (req, res) => {
     const client = await pool.connect()
     await addtokimon(req.body,client)
     res.send('ok')
     client.release();
   })
-	.patch('/test/:id', async (req, res) => {
+	.patch('/mainpage/:id', async (req, res) => {
 		const client = await pool.connect()
 		await changetokimon(req.body, req.params.id, client)
 		res.send('ok')
 		client.release();
 	})
-	.delete('/test/:id', async (req, res) => {
+	.delete('/mainpage/:id', async (req, res) => {
 		const client = await pool.connect()
 		await deletetokimon(req.params.id, client)
 		res.send('ok')
+		client.release();
+	})
+	.get('/mainpage/:id', async (req, res) => {
+		const client = await pool.connect()
+		const text = 'SELECT * FROM Tokimon WHERE name = $1'
+		const value = [req.params.id]
+		const specific = await client.query(text, value)
+		const results = {'results': specific.rows};
+		res.render('pages/specificTokimon', results)
 		client.release();
 	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
